f55cae0b057c58b940c27bce3176eb7cf35afbfa
ngxaki69 <90611779+ngxaki69@users.noreply.github.com>
2024-07-11 13:10:07 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,10 +2,15 @@ import logging
 import os
 import sys
 import requests
+import simplejson
 import time
 
+
+from contextlib import suppress
 from dotenv import load_dotenv
+from http import HTTPStatus
 from telebot import TeleBot
+from telebot.apihelper import ApiException
 
 
 load_dotenv()
@@ -51,8 +56,11 @@ def check_tokens():
 
 def send_message(bot, message):
     """Функция отправки сообщения пользователю."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
-    logging.debug('Бот отравил сообщение пользователю.')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.debug('Бот отравил сообщение пользователю.')
+    except ApiException as e:
+        logging.error(f'Ошибка при отправке сообщения: {e}')
 
 
 def get_api_answer(timestamp):
@@ -60,19 +68,29 @@ def get_api_answer(timestamp):
     params = {'from_date': timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        if response.status_code != 200:
+        if response.status_code != HTTPStatus.OK:
             logging.error(f'Произошла ошибка при запросе:\
                           {response.status_code}')
             raise
         return response.json()
-    except requests.RequestException:
-        return None
+    except requests.RequestException as e:
+        logging.error(f'Ошибка при отправке запроса: {e}')
+        # когда я тут делаю raise у меня не проходит тест
+        # AssertionError: Убедитесь, что в функции `get_api_answer`
+        # обрабатывается ситуация, когда при запросе к API возникает
+        # исключение `requests.RequestException`.
+    except simplejson.errors.JSONDecodeError as e:
+        logging.error(f'Ошибка при форматировании JSON: {e}')
+        raise
 
 
 def check_response(response):
     """Функция проверки наличия работ."""
-    if 'homeworks' not in response or not isinstance(response['homeworks'],
-                                                     list):
+    if not isinstance(response, dict) or \
+            'homeworks' not in response or \
+            'current_date' not in response or \
+            not isinstance(response['homeworks'], list) or \
+            not isinstance(response['current_date'], int):
         raise TypeError('Неверный ответ от API')
     return response['homeworks']
 
@@ -81,6 +99,8 @@ def parse_status(homework):
     """Функция проверки статуса у работы."""
     if 'homework_name' not in homework:
         raise KeyError('Отсутствует название работы.')
+    if 'status' not in homework:
+        raise KeyError('Отсутствует статус работы.')
     status = homework['status']
     if status not in HOMEWORK_VERDICTS:
         raise ValueError(f'Unexpected status value: {status}')
@@ -96,14 +116,17 @@ def main():
     # Создаем объект класса бота
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    last_status = None
     while True:
         try:
             response = get_api_answer(timestamp)
             logging.debug('Запрос на API прошел успешно.')
             homeworks = check_response(response)
-            if homeworks:
+            if homeworks and homeworks[0]['status'] != last_status:
                 message = parse_status(homeworks[0])
-                send_message(bot, message)
+                with suppress(Exception):
+                    send_message(bot, message)
+                last_status = homeworks[0]['status']
             timestamp = response.get('current_date', timestamp)
         except Exception as error:
             logging.error(f'Ошибка в работе main функции: {error}')
@@ -115,3 +138,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
